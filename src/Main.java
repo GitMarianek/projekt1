0f63d868f5d552c2ffe6af05539bdc586fccd91d
frytki <frytkimajonez@example.com>
2022-04-20 17:01:43 +0200

    update

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,18 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-      Scanner scanner = new Scanner(System.in);
-      System.out.println("Jak masz na imię?");
-      String name = scanner.nextLine();
-      System.out.println("Jak masz na nazwisko?");
-      String surname = scanner.nextLine();
-       System.out.println("Ile masz lat?");
-       String age = scanner.nextLine();
-      System.out.println("cześć, "  + name + " " + surname + " " + age);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Jak masz na imię?");
+        String name = scanner.nextLine();
+        System.out.println("Jak masz na nazwisko?");
+        String surname = scanner.nextLine();
+        System.out.println("Cześć, " + name + " " + surname);
+        System.out.println("Ile masz lat?");
+
+        int age = scanner.nextInt();
+        if (age > 18){System.out.println("Jesteś pełnoletni!");}
+        else {System.out.println("Jesteś niepełnoletni!");}
+
 
 
     }
